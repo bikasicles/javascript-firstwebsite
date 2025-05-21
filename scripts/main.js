2c45984c331b916f6c6dbdf722aeb63bde7c1949
bikasicles <liambj007@gmail.com>
2025-05-21 17:46:23 -0600

    finished my first section of my website

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@ myImage.addEventListener("click", () => {
     }
 });
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `JavaScript is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `JavaScript is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
